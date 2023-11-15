d055dc995a2caa943847de3417a437c0807424fc
Donato Martinelli <martinellidonato06gmail.com>
2023-11-15 13:13:31 +0100

    rearranged folders

---
+++ Decision Tree and Random Forest/decistionTreeRandomForest.py
@@ -0,0 +1,166 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# Load the Melbourne housing data
+melbourne_file_path = 'C:/Users/donat/Desktop/melb_data.csv'
+data = pd.read_csv(melbourne_file_path) 
+
+# Separate the target (y) and predictors (X)
+y = data.Price
+predictors = data.drop(['Price'], axis=1)
+
+# Split predictors into numerical and categorical features
+X_numerical = predictors.select_dtypes(exclude=['object'])
+X_categorical = predictors.select_dtypes('object')
+
+# Identify categorical columns
+variables = (X_categorical.dtypes == 'object')
+object_cols = list(variables[variables].index)
+
+# Calculate cardinality for each categorical variable
+var_card = []
+for variable in object_cols:
+    unique_values = X_categorical[variable].unique()
+    cardinality = len(unique_values)
+    var_card.append(cardinality)
+
+# Create a DataFrame to display variable cardinality
+df = pd.DataFrame({'Variable': [var for var in object_cols], 'Cardinality': [car for car in var_card]})
+
+# Define columns to one-hot encode
+categorical_cols_to_one_hot_encode = ['Type', 'Method', 'Regionname']
+X_categorical_to_encode = X_categorical[categorical_cols_to_one_hot_encode]
+
+# One-hot encode the selected categorical columns
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols = pd.DataFrame(OH_encoder.fit_transform(X_categorical_to_encode))
+OH_cols.index = X_categorical_to_encode.index
+OH_cols.columns = OH_cols.columns.astype(str)
+
+# Drop original categorical columns that were one-hot encoded
+X_categorical = X_categorical.drop(categorical_cols_to_one_hot_encode, axis=1)
+
+# Concatenate one-hot encoded columns with the original categorical columns
+X_categorical_encoded = pd.concat([X_categorical, OH_cols], axis=1)
+X_categorical_encoded.columns = X_categorical_encoded.columns.astype(str)
+
+# Define columns to perform frequency encoding
+categorical_cols_to_frequency_encode = ['Suburb', 'Address', 'SellerG', 'Date', 'CouncilArea']
+
+# Perform frequency encoding on selected categorical columns
+for col in categorical_cols_to_frequency_encode:
+    freq = X_categorical[col].value_counts(normalize=True)
+    X_categorical[col+'_freq'] = X_categorical[col].map(freq)
+
+# Drop original categorical columns used for frequency encoding
+X_categorical = X_categorical.drop(categorical_cols_to_frequency_encode, axis=1)
+
+# Concatenate one-hot encoded columns with the original categorical columns
+X_categorical_encoded = pd.concat([X_categorical, OH_cols], axis=1)
+
+# Concatenate one-hot encoded and numerical features
+X = pd.concat([X_categorical_encoded, X_numerical], axis=1)
+
+# Display the merged DataFrame
+print(X)
+
+# Count missing values in each column
+missing_val_count_by_column = (X.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+# Split the data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+# Identify columns with missing values
+cols_with_missing = [col for col in X_train.columns if X_train[col].isnull().any()]
+
+# Make copies to avoid changing the original data during imputation
+X_train_plus = X_train.copy()
+X_valid_plus = X_valid.copy()
+
+# Create new columns indicating what will be imputed
+for col in cols_with_missing:
+    X_train_plus[col + '_was_missing'] = X_train_plus[col].isnull()
+    X_valid_plus[col + '_was_missing'] = X_valid_plus[col].isnull()
+
+# Impute missing values
+my_imputer = SimpleImputer()
+imputed_X_train_plus = pd.DataFrame(my_imputer.fit_transform(X_train_plus))
+imputed_X_valid_plus = pd.DataFrame(my_imputer.transform(X_valid_plus))
+imputed_X_train_plus.columns = X_train_plus.columns
+imputed_X_valid_plus.columns = X_valid_plus.columns
+
+# Define a function to calculate Mean Absolute Error (MAE) for Random Forest Regressor
+def get_mae_rf(n_estimators, imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=n_estimators, random_state=0)
+    model.fit(imputed_X_train_plus, y_train)
+    preds_val = model.predict(imputed_X_valid_plus)
+    mae = mean_absolute_error(y_valid, preds_val)
+    return mae
+# Test different values of n_estimators and print their MAE
+print("Testing different n_estimators:")
+estimators, mae_rf = [], []
+number_of_estimators = [10, 50, 100, 200]
+print("Running tests for optimal number of estimators")
+for n_estimators in number_of_estimators:
+    my_mae = get_mae_rf(n_estimators, imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid)
+    estimators.append(n_estimators)
+    #print("n_estimators: %d  \t\t Mean Absolute Error: %d" % (n_estimators, my_mae))
+    mae_rf.append(my_mae)
+# Create a dictionary to store n_estimators as keys and their corresponding MAEs as values
+pairs = {estimators[i]: mae_rf[i] for i in range(len(estimators))}
+# Find the key with the lowest value (i.e., the best n_estimators value) using min() and a custom key function
+optimal_n_estimators = min(pairs, key=pairs.get)
+print("Optimal n_estimators:", optimal_n_estimators)
+# Train the model and make predictions
+model = RandomForestRegressor(n_estimators = optimal_n_estimators, random_state=0)
+model.fit(imputed_X_train_plus, y_train)
+randomForestPredictions = model.predict(imputed_X_valid_plus)
+mae_rf = mean_absolute_error(y_valid, randomForestPredictions)
+print(f"Random Forest MAE: {mae_rf}")
+
+# Define a function to calculate Mean Absolute Error (MAE) for Decision Tree Regressor
+def get_mae_dt(max_leaf_nodes, imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(imputed_X_train_plus, y_train)
+    preds_val = model.predict(imputed_X_valid_plus)
+    mae = mean_absolute_error(y_valid, preds_val)
+    return(mae)
+# Test different values of max_leaf_nodes and print their MAE
+print("Testing different max_leaf_nodes:")
+nodes, mae_dt = [], []
+number_of_leaf_nodes = [5, 50, 500, 5000]
+for max_leaf_nodes in number_of_leaf_nodes:
+    my_mae = get_mae_dt(max_leaf_nodes, imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid)
+    nodes.append(max_leaf_nodes)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+    mae_dt.append(my_mae)
+# Create a dictionary to store max_leaf_nodes as keys and their corresponding MAEs as values
+pairs = {nodes[i]: mae_dt[i] for i in range(len(nodes))}
+# Find the key with the lowest value (i.e., the best max_leaf_nodes value) using min() and a custom key function
+key_with_lowest_mae = min(pairs, key=pairs.get)
+print("Key with lowest MAE:", key_with_lowest_mae)
+# Extract keys and values from the dictionary
+keys, values = list(pairs.keys()), list(pairs.values())
+# Decision Tree 
+melbourne_model = DecisionTreeRegressor(max_leaf_nodes=key_with_lowest_mae, random_state=1)
+melbourne_model.fit(imputed_X_train_plus, y_train)
+decisionTreePredictions = melbourne_model.predict(imputed_X_valid_plus)
+mae_dt = mean_absolute_error(y_valid, decisionTreePredictions)
+print(f"Decision Tree MAE: {mae_dt}")
+
+# Create a DataFrame to compare actual vs. predicted results
+comparison_df = pd.DataFrame({'Actual': y_valid,
+                               'RandomForestPredictions': randomForestPredictions,
+                               'DecisionTreePrediction': decisionTreePredictions})
+comparison_df['RandomForestPredictions'] = comparison_df['RandomForestPredictions'].round(2)
+comparison_df['DecisionTreePrediction'] = comparison_df['DecisionTreePrediction'].round(2)
+
+# Present the model and the results
+print("Comparison of Actual vs. Predicted Results:\n", comparison_df.head())
---
+++ Iowa Competition/maincode.py
@@ -0,0 +1,285 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.feature_selection import mutual_info_regression
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+random_seed = 0
+np.random.seed(random_seed)
+
+# Setting display options for pandas
+#pd.set_option('display.max_columns', 200)  # Setting the maximum number of displayed columns to 200
+pd.set_option('display.max_rows', 200)     # Setting the maximum number of displayed rows to 200
+
+# Importing warnings module and setting to ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+def dataset_summary(datasets, display_columns, display_dtype, display_statistics):
+    """
+    This function summarizes a dataset by providing essential information such as dataset shape,
+    total cells, missing data statistics, columns with missing values, and data type counts.
+    
+    Parameters:
+    datasets (DataFrame): The dataset to be summarized.
+    display_columns (str): A flag indicating whether to display dataset columns or not ("y" for yes, "n" for no).
+    display_dtype (str): A flag indicating whether to display dtype for each column or not ("y" for yes, "n" for no).
+    display_statistics (str): A flag indicating whether to display summary statitsics or not ("y" for yes, "n" for no).
+    """
+
+    # Display the entire dataset
+    print("Dataset:")
+    print(datasets)
+
+    # Get dataset columns and shape
+    dataset_columns = list(datasets.columns)
+    dataset_shape = datasets.shape
+
+    # Calculate missing data statistics
+    missing_data_per_column = datasets.isnull().sum()
+    total_cells = np.product(dataset_shape)
+    total_missing = missing_data_per_column.sum()
+    percent_missing = (total_missing / total_cells) * 100
+
+    # Display dataset shape and missing data statistics
+    print("------------------------------------")
+    print(f"Dataset Shape: {dataset_shape}")
+    print(f"Total Cells: {total_cells}")
+    print(f"Total Missing: {total_missing}")
+    print(f"Percentage of Missing Data: {percent_missing:.2f}%")
+    print("------------------------------------")
+
+    # Display columns with missing values and their counts
+    print("Columns with Missing Values:")
+    for column in dataset_columns:
+        if datasets[column].isnull().sum() > 0:
+            print(f" {column}, Missing Values: {datasets[column].isnull().sum()}")
+
+    # Count the occurrence of each data type
+    dataset_datatypes = {}
+    for column in dataset_columns:
+        data_type = datasets[column].dtype
+        if data_type in dataset_datatypes:
+            dataset_datatypes[data_type] += 1
+        else:
+            dataset_datatypes[data_type] = 1
+
+    # Display dataset datatypes with their counts
+    print("------------------------------------")
+    print("Dataset Datatypes with Counts:")
+    for data_type, count in dataset_datatypes.items():
+        print(f" {data_type}: {count}")
+    print("------------------------------------")
+
+    if display_columns == "y":
+        # Display dataset columns
+        print("Dataset Columns:")
+        for column in dataset_columns:
+            print(column)
+    if display_dtype == "y":
+        # Display dataset columns
+        print("Dataset Columns Data Types:")
+        print(datasets.info())
+    if display_statistics == "y":
+        # Display dataset columns
+        print("Dataset Summary Statistics:")
+        print(train.describe().T)
+
+    return ""
+
+train_filepath = "C:/Users/marti/Desktop/projects/House-Prices--Advanced-Regression-Techniques/train.csv"
+test_filepath = "C:/Users/marti/Desktop/projects/House-Prices--Advanced-Regression-Techniques/test.csv"
+train= pd.read_csv(train_filepath)
+test= pd.read_csv(test_filepath)
+
+print("-"*50)
+print("INITIAL STATE OF THE DATASET")
+print("-"*50)
+print(dataset_summary(train, display_columns="n", display_dtype="y", display_statistics="y"))
+
+
+# Drop the 'Id' column and modify the DataFrame in place
+train.drop(columns=['Id'], inplace=True)
+
+# Drop duplicate rows from the DataFrame in place
+train.drop_duplicates(inplace=True)
+
+train['LotFrontage'].fillna(0, inplace=True)
+train['MasVnrArea'].fillna(0, inplace=True)
+train['GarageYrBlt'].fillna(0, inplace=True)
+
+columns_to_original_encode = ['GarageType', 'LotShape', 'LandContour', 
+                              'LandSlope', 'ExterQual', 'ExterCond', 
+                              'BsmtQual', 'BsmtCond', 'BsmtExposure',
+                              'BsmtFinType1', 'BsmtFinType2', 'HeatingQC',
+                              'KitchenQual', 'FireplaceQu', 'GarageFinish',
+                              'GarageQual', 'GarageCond', 'PoolQC',
+                              'Fence']
+
+# Initialize the ordinal encoder
+encoder = OrdinalEncoder()
+for column in columns_to_original_encode:
+
+    # Handle missing values by replacing with "NA" and then encoding
+    train[column].fillna("NA", inplace=True)
+    train[column] = encoder.fit_transform(train[[column]])
+
+columns_to_frequency_encode = ['MSZoning', 'Utilities', 'LotConfig',
+                               'Neighborhood', 'Condition1', 'Condition2',
+                               'BldgType', 'HouseStyle', 'RoofStyle',
+                               'RoofMatl', 'Exterior1st', 'Exterior2nd',
+                               'MasVnrType', 'Foundation', 'Heating',
+                               'Electrical', 'Functional', 'MiscFeature',
+                               'SaleType', 'SaleCondition']
+
+for column in columns_to_frequency_encode:
+    train[column].fillna("NA", inplace=True)
+
+    # Calculate frequency of each category in the current column
+    category_frequencies = train[column].value_counts(normalize=True)
+
+    # Replace categories with their frequencies
+    train[column] = train[column].map(category_frequencies)
+
+
+columns_to_one_hot_encode = ['Street', 'Alley', 'PavedDrive', 'CentralAir']
+
+# Create an empty DataFrame to store the one-hot encoded columns
+one_hot_encoded_df = pd.DataFrame()
+for column in columns_to_one_hot_encode:
+    train[column].fillna("NA", inplace=True)
+
+    # Apply one-hot encoding to the current column
+    one_hot_encoded_column = pd.get_dummies(train[column], prefix=column, prefix_sep='_')
+
+    # Concatenate the one-hot encoded column with the new DataFrame
+    one_hot_encoded_df = pd.concat([one_hot_encoded_df, one_hot_encoded_column], axis=1)
+
+# Convert boolean values to integers (0s and 1s)
+one_hot_encoded_df = one_hot_encoded_df.astype(int)
+
+# Concatenate the one-hot encoded DataFrame with the original DataFrame
+train = pd.concat([train, one_hot_encoded_df], axis=1)
+
+# Drop the original columns that were one-hot encoded
+train.drop(columns=columns_to_one_hot_encode, inplace=True)
+
+print("-"*50)
+print("STATE OF THE DATASET AFTER CATEGORICAL ENCODING AND HANDLING MISSING VALUES")
+print("-"*50)
+print(dataset_summary(train, display_columns="n", display_dtype="y", display_statistics="y"))
+
+#subset containig all original numerical features plus those who then went through original and freuqency encoding
+df_num = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 
+          'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 
+          '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 
+          'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 
+          'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', 
+          '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold','GarageType', 
+          'LotShape', 'LandContour', 'LandSlope', 'ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 
+          'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'HeatingQC', 'KitchenQual', 'FireplaceQu', 
+          'GarageFinish', 'GarageQual', 'GarageCond', 'PoolQC','Fence', 'MSZoning', 'Utilities', 'LotConfig',
+          'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle',
+          'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation', 'Heating',
+          'Electrical', 'Functional', 'MiscFeature', 'SaleType', 'SaleCondition', 'SalePrice']
+subset_df = train.loc[:,df_num]
+
+
+# Display descriptive statistics for the 'SalePrice' column
+print("Descriptive statistics for the 'SalePrice' column")
+print(train['SalePrice'].describe())
+
+# Plot a histogram for the 'SalePrice' column using seaborn
+sns.distplot(train['SalePrice'], hist_kws={'alpha': 0.4}, bins=100, color='r')
+plt.show()
+
+# Calculate the correlation matrix for numerical features
+print("Train Correlation")
+print(subset_df.corr())
+
+# Plot a heatmap of the correlation matrix
+cormat = subset_df.corr()
+paper = plt.figure(figsize=(7, 8))
+sns.set(font_scale=1.2)
+sns.heatmap(cormat, cmap="coolwarm", cbar=True, vmax=1, square=True)
+plt.title("Correlation Heatmap")
+plt.show()
+
+# Plot histograms for numerical features
+#subset_df.hist(figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8, color='Red')
+#plt.show()
+
+# Select the best numerical features based on correlation with 'SalePrice'
+subset_df_corr = subset_df.corr()['SalePrice'][:-1]
+best_num_features = subset_df_corr[abs(subset_df_corr) > 0.4].sort_values(ascending=False)
+print(f"There are {len(best_num_features)} best features with SalePrice:\n\n{best_num_features}")
+
+# Calculate the correlation matrix for numerical features
+correlation_matrix = subset_df.corr()
+sales_price_corr = correlation_matrix["SalePrice"].sort_values(ascending=False)
+sale_price_corr_df = pd.DataFrame(sales_price_corr)
+
+# Plot a heatmap of correlation with SalePrice
+plt.figure(figsize=(10, 8))
+sns.heatmap(sale_price_corr_df, annot=True, cmap="coolwarm", cbar=True)
+plt.xlabel('Features')
+plt.ylabel('Features')
+plt.title('Correlation Heatmap with SalePrice')
+plt.show()
+
+# Initialize a StandardScaler to standardize the numerical features
+scaler = StandardScaler()
+
+# Standardize the numerical features in the DataFrame 'df_num'
+df_num_scaled = scaler.fit_transform(subset_df)
+
+# Define the target column for which we will calculate mutual information
+target_column = 'SalePrice'
+
+# Calculate mutual information between standardized numerical features and the target
+mi = mutual_info_regression(df_num_scaled, subset_df[target_column])
+
+# Create a Series to store mutual information with feature names as indices
+mi_series = pd.Series(mi, index=subset_df.columns)
+
+# Print mutual information in descending order
+print("Mutual Information:")
+print(mi_series.sort_values(ascending=False))
+
+
+features_train_A = ['OverallQual', 'GrLivArea' ,'GarageCars', 'TotalBsmtSF', 'GarageArea', 
+                   'YearBuilt', 'Neighborhood','BsmtQual', 'KitchenQual', 'ExterQual' ,
+                   '1stFlrSF', 'MSSubClass', 'GarageFinish', 'FullBath', 'GarageYrBlt',
+                   'YearRemodAdd', 'TotRmsAbvGrd', 'SalePrice']
+train_A = train.loc[:, features_train_A]
+print()
+
+
+"""
+At this point, i have a dataframe containing the best features for both mutual information rate and correlation rate
+you have to calculate this also for cat variables that were frequency and original encoded, not oe-hot encoded
+then modify the best features for both mutual information rate and correlation rate
+
+you have to just structure the code better to make it more readable, 
+then understand the remaining lines what asks you to plot
+
+then build 3 neural networks and more models 
+
+one with the best features + onehot encoded variables
+one with the best features 
+
+work with each single neural network and hyperparameter tune them
+
+once you're done, compare the results and submit the results of the best one, then prepare a pdf file and post it on your linkedin profile
+then send it to the Quant team and asks for suggestions
+
+then congrats, the project is done (you'll come back eventually to optimize it but you can be over with it)
+
+"""
+
+
+
+
